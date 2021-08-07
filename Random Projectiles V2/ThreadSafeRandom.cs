9b4ac6ae3400c7a34807807066f7716d5a5e8149
Matt <2matthew05@gmail.com>
2021-08-06 20:24:02 -0500

    Fix some minor issues

--- Random Projectiles V2/ThreadSafeRandom.cs
+++ Random Projectiles V2/ThreadSafeRandom.cs
@@ -1,33 +1,14 @@
-﻿using UnhollowerBaseLib;
-
-namespace Random_Projectiles_V2 {
+﻿namespace Random_Projectiles_V2 {
     public static class ThreadSafeRandom {
-        public static T GetRandomObjectFromList<T>(this Il2CppSystem.Collections.Generic.List<T> list) {
-            return list[URandom.GetNextInt(list.Count)];
-        }
         public static T GetRandomObjectFromList<T>(this System.Collections.Generic.List<T> list) {
-            return list[URandom.GetNextInt(list.Count)];
-        }
-        public static T GetRandomObjectFromList<T>(this Il2CppReferenceArray<T> list) where T : Il2CppObjectBase {
-            return list[URandom.GetNextInt(list.Count)];
+            return list[MelonMod_Main.__randomInstance.GetNextInt(list.Count)];
         }
 
         public static void Shuffle<T>(this System.Collections.Generic.List<T> list) {
             int n = list.Count;
             while (n > 1) {
                 n--;
-                int k = URandom.GetNextInt(n + 1);
-                T value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-        }
-
-        public static void Shuffle<T>(this Il2CppSystem.Collections.Generic.List<T> list) {
-            int n = list.Count;
-            while (n > 1) {
-                n--;
-                int k = URandom.GetNextInt(n + 1);
+                int k = MelonMod_Main.__randomInstance.GetNextInt(n + 1);
                 T value = list[k];
                 list[k] = list[n];
                 list[n] = value;
--- Random Projectiles V2/URandom.cs
+++ Random Projectiles V2/URandom.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 
 namespace Random_Projectiles_V2 {
-    internal static class URandom {
-        internal static ulong lastRand = GetNext((ulong)DateTime.Now.ToUniversalTime().TimeOfDay.TotalMilliseconds);
+    internal class URandom {
+        internal ulong lastRand = (ulong)DateTime.Now.ToUniversalTime().TimeOfDay.TotalMilliseconds;
 
-        internal static ulong GetNext(ulong max = 0UL) {
+        internal ulong GetNext(ulong max = 0UL) {
             var key = new[] { lastRand / 2, lastRand / 3, lastRand / 4, lastRand / 5, lastRand / 6, lastRand / 7, lastRand / 8, lastRand / 9, lastRand / 10 };
             var v = lastRand & 0xFFFFFFFFFFFFul;
             var v0 = (v >> 24) & 0xFFFFFFu;
@@ -18,25 +18,11 @@ namespace Random_Projectiles_V2 {
                 v1 += (((v0 << 3) ^ (v0 >> 4)) + v0) ^ (sum + key[(sum >> 8) & 3]);
                 v1 &= 0xFFFFFFu;
             }
-
-            return lastRand = ((v0 << 24) + v1) % max == 0UL ? ulong.MaxValue : max;
+            var ret = ((v0 << 24) + v1) % max == 0UL ? ulong.MaxValue-1 : max;
+            lastRand = ret;
+            return ret;
         }
 
-        internal static int GetNextInt(int max = 0) {
-            var key = new[] { lastRand / 2, lastRand / 3, lastRand / 4, lastRand / 5, lastRand / 6, lastRand / 7, lastRand / 8, lastRand / 9, lastRand / 10 };
-            var v = lastRand & 0xFFFFFFFFFFFFul;
-            var v0 = (v >> 24) & 0xFFFFFFu;
-            var v1 = (v & 0xFFFFFFu);
-            var sum = 0u;
-            for (var i = 32 - 1; i >= 0; --i) {
-                v0 += (((v1 << 3) ^ (v1 >> 4)) + v1) ^ (sum + key[sum & 3]);
-                v0 &= 0xFFFFFFu;
-                sum += 0xF1BFC;
-                sum &= 0xFFFFFFu;
-                v1 += (((v0 << 3) ^ (v0 >> 4)) + v0) ^ (sum + key[(sum >> 8) & 3]);
-                v1 &= 0xFFFFFFu;
-            }
-            return Convert.ToInt32(lastRand = ((v0 << 24) + v1) % Convert.ToUInt64(max == 0 ? int.MaxValue-1 : max));
-        }
+        internal int GetNextInt(int max = 0) => Convert.ToInt32(GetNext(Convert.ToUInt64(max)) % int.MaxValue-1);
     }
 }
